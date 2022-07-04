aeedbb60b0bbf4ff729378471d1facf5124fe132
metinbeycigim <metinbaydere@gmail.com>
2022-07-04 17:31:59 -0500

    login page UI has been created

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,58 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Well Organized',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Well Organized')),
+        body: Column(
+          children: [
+            Container(
+              height: 150.0,
+              width: 190.0,
+              padding: const EdgeInsets.only(top: 40),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(200),
+              ),
+              child: const Center(
+                child: Placeholder(child: Text('logo gelecek')),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Enter valid mail id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your secure password'),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration:
+                  BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
